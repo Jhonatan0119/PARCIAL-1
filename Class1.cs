dd3b10088fce19126f6f916ab3d201576974a0e1
MINEDUCYT <MINEDUCYT@DESKTOP-NFDMETJ>
2022-09-02 14:58:23 -0600

    Agregar archivos de proyecto.

---
+++ Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c
+{
+
+    class iEstudiantes
+    {
+        private int id;
+        private String nombre;
+        private int edad;
+        private int nota1;
+        private int nota2;
+        private int parcial;
+        private int notafinal;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+        public int Nota1
+        {
+            get { return nota1; }
+            set { nota1 = value; }
+        }
+
+        public int Nota2
+        {
+            get { return nota2; }
+            set { nota2 = value; }
+        }
+
+        public int Parcial
+        {
+           get { return parcial; }
+            set { parcial = value; }
+        }
+
+
+        public int NotaFinal
+        {
+            get { return notafinal; }
+            set { notafinal = value; }
+        }
+
+
+
+        public iEstudiantes(int id,  string nombre, int edad, int nota1, int nota2, int parcial, int notafinal)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.edad = edad;
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+            this.parcial = parcial;
+            this.notafinal = notafinal;
+
+
+        }
+
+        public string Mostrar()
+        {
+            return "ESTUDIANTE: " + nombre + " " + " EDAD: " + edad + " NOTA FINAL: " + notafinal + " ESTADO: ";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using c;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARCIAL_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Id = 0;
+            String Nombre;
+            int Edad;
+            String control = "Si";
+            String control2 = "Si";
+            int indice = 0;
+            int Nota1;
+            int Nota2;
+            int Parcial;
+            int NotaFinal;
+            int Opciones;
+            string Cerrar;
+
+            iEstudiantes[] Infoestudiantes = new iEstudiantes[1];
+
+
+            while (control == "Si")
+            {
+                Console.WriteLine("Ingrese los siguientes datos:");
+                Console.WriteLine("Ingrese el Nombre:");
+                Nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese la Edad:");
+                Edad = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la Nota1:");
+                Nota1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la Nota2:");
+                Nota2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la Nota del Parcial:");
+                Parcial = int.Parse(Console.ReadLine());
+
+                NotaFinal = (int)((Nota1 * .3)+ (Nota2 * .3) +(Parcial * .4));
+
+                Infoestudiantes[indice] = new iEstudiantes(Id, Nombre, Edad, Nota1, Nota2, Parcial, NotaFinal);
+                Id = Id + 1;
+                Console.WriteLine("Desea agregar otro registro (Si, No): ");
+                control = Console.ReadLine();
+                if (control == "Si")
+                {
+                    indice = indice + 1;
+                    Array.Resize(ref Infoestudiantes, Infoestudiantes.Length + 1);
+
+                }
+                else { control = "No"; }
+            }
+
+
+            while (control2 == "Si")
+            {
+                Console.WriteLine("Desea consultar por Aprobados: Digite 1");
+                Console.WriteLine("Desea consultar por Reprobados: Digite 2");
+                Opciones = int.Parse(Console.ReadLine());
+                if (Opciones == 1)
+                {
+                    IEnumerable<iEstudiantes> reqEstudiantes = from estudiante in Infoestudiantes
+                                                                   where estudiante.NotaFinal >= 6
+                                                                   select estudiante;
+                    foreach (iEstudiantes consulta in reqEstudiantes)
+                    {
+                        Console.WriteLine(consulta.Mostrar() + "¡APROBADO!");
+
+                    }
+                }
+                else if (Opciones == 2)
+                {
+                    IEnumerable<iEstudiantes> reqEstudiantes = from estudiante in Infoestudiantes
+                                                                   where estudiante.NotaFinal < 6
+                                                                   select estudiante;
+                    foreach (iEstudiantes consulta in reqEstudiantes)
+                    {
+                        Console.WriteLine(consulta.Mostrar() + "¡REPROBADO!");
+
+                    }
+                }
+              
+                Console.WriteLine("Desea continuar: Si, No");
+                Cerrar = Console.ReadLine();
+                if (Cerrar == "No")
+                {
+                    control2 = "No";
+                }
+            }
+
+            Console.Read();
+
+        }
+    }
+}
